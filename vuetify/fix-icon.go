816749425e80ce392dfa4f9ee04779a0d1129279
Felix Sun <sunfmin@gmail.com>
2024-03-19 08:35:15 +0800

    Fix icon builder to new api

--- vuetify/fix-icon.go
+++ vuetify/fix-icon.go
@@ -1,10 +1,7 @@
 package vuetify
 
-import h "github.com/theplant/htmlgo"
-
 func VIcon(name string) (r *VIconBuilder) {
-	r = &VIconBuilder{
-		tag: h.Tag("v-icon").Text(name),
-	}
+	r = &VIconBuilder{}
+	r.Icon(name)
 	return
 }
